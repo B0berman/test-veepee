f60536fa8a96f478340546a7e69b54a09a89e8ff
Mahmoud <mbhaya@vente-privee.com>
2018-10-22 10:22:33 +0200

    Add detail module changes

--- features/detail/src/main/java/com/vp/detail/DetailActivity.kt
+++ features/detail/src/main/java/com/vp/detail/DetailActivity.kt
@@ -1,12 +1,19 @@
 package com.vp.detail
 
+import android.arch.lifecycle.LiveData
 import android.arch.lifecycle.Observer
 import android.arch.lifecycle.ViewModelProvider
 import android.arch.lifecycle.ViewModelProviders
+import android.content.Intent
 import android.databinding.DataBindingUtil
+import android.net.Uri
 import android.os.Bundle
 import android.view.Menu
+import android.view.MenuItem
+import android.widget.Button
+import android.widget.Toast
 import com.vp.detail.databinding.ActivityDetailBinding
+import com.vp.detail.model.MovieDetail
 import com.vp.detail.viewmodel.DetailsViewModel
 import dagger.android.support.DaggerAppCompatActivity
 import javax.inject.Inject
@@ -35,8 +42,29 @@ class DetailActivity : DaggerAppCompatActivity(), QueryProvider {
         return true
     }
 
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        // Handle presses on the action bar menu items
+        if (item.itemId == R.id.star)
+        {
+            var url = "app://movies/favorites?addRequest=true&imdbID=" + getMovieId() + "&poster=" + getPoster()
+            var toFavoriesIntent = Intent(Intent.ACTION_VIEW, Uri.parse(url))
+            toFavoriesIntent.setPackage(this.packageName)
+            Toast.makeText(this, getMovieId(), Toast.LENGTH_LONG).show()
+            startActivity(toFavoriesIntent)
+            finish()
+        }
+
+        return super.onOptionsItemSelected(item)
+    }
+
     override fun getMovieId(): String {
         return intent?.data?.getQueryParameter("imdbID") ?: run {
+            throw IllegalStateException("You must provide movie id to display details")
+        }
+    }
+
+    fun getPoster(): String {
+        return intent?.data?.getQueryParameter("poster") ?: run {
             throw IllegalStateException("You must provide movie id to display details")
         }
     }
