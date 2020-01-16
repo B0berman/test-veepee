12873184a344b00aacb7e5b62b5e83cf2dbc0d64
Gabriel Ferreira <gabriel@vrp.com.br>
2020-01-16 17:27:39 +0100

    Final fixes and removing empty instrumented test

--- features/list/src/main/java/com/vp/list/model/SearchResponse.kt
+++ features/list/src/main/java/com/vp/list/model/SearchResponse.kt
@@ -2,19 +2,8 @@ package com.vp.list.model
 
 import com.google.gson.annotations.SerializedName
 
-class SearchResponse private constructor(
-    @SerializedName("Response")
-    val response: String,
+class SearchResponse constructor(
     @SerializedName("Search")
-    val search: MutableList<ListItem>,
+    val search: List<ListItem>,
     val totalResults: Int
-) {
-
-    companion object {
-        private val POSITIVE_RESPONSE: String = "True"
-    }
-
-    fun hasResponse(): Boolean {
-        return POSITIVE_RESPONSE == response
-    }
-}
+)
