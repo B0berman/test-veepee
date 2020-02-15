66d5049d999ad978c85131d6a9774795df850756
Tushar Saha <tushar.saha@schibsted.com>
2020-02-15 17:32:42 +0100

    Fixed 'The Wrong State' issue. Added Unit test

--- features/list/src/test/java/com/vp/list/viewmodel/ListViewModelTest.java
+++ features/list/src/test/java/com/vp/list/viewmodel/ListViewModelTest.java
@@ -8,15 +8,21 @@ import com.vp.list.service.SearchService;
 
 import org.junit.Rule;
 import org.junit.Test;
+import org.mockito.ArgumentCaptor;
 
 import java.io.IOException;
+import java.util.List;
 
 import retrofit2.mock.Calls;
 
+import static kotlin.text.Typography.times;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.Assert.assertEquals;
 import static org.mockito.ArgumentMatchers.anyInt;
+import static org.mockito.ArgumentMatchers.anyList;
 import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
@@ -52,6 +58,34 @@ public class ListViewModelTest {
 
         //then
         verify(mockObserver).onChanged(SearchResult.inProgress());
+    }
+
+    @Test
+    public void shouldReturnSuccessState() {
+        //given
+        SearchService searchService = mock(SearchService.class);
+        when(searchService.search(anyString(), anyInt())).thenReturn(Calls.response(mock(SearchResponse.class)));
+        ListViewModel listViewModel = new ListViewModel(searchService);
+
+        Observer<SearchResult> mockObserver = (Observer<SearchResult>) mock(Observer.class);
+        listViewModel.observeMovies().observeForever(mockObserver);
+
+        //when
+        listViewModel.searchMoviesByTitle("title", 1);
+
+        ArgumentCaptor<SearchResult> listStateArgumentCaptor = ArgumentCaptor.forClass(SearchResult.class);
+        // state should change 2 times
+        // record state change
+        verify(mockObserver, times(2)).onChanged(listStateArgumentCaptor.capture());
+        assertEquals(2, listStateArgumentCaptor.getAllValues().size());
+
+        List<SearchResult> states = listStateArgumentCaptor.getAllValues();
+        // check the expected state change sequence
+        assertThat(states.get(0).getListState()).isEqualTo(ListState.IN_PROGRESS);
+        assertThat(states.get(1).getListState()).isEqualTo(ListState.LOADED);
+
+        //then check the final state
+        assertThat(listViewModel.observeMovies().getValue().getListState()).isEqualTo(ListState.LOADED);
     }
 
 }
