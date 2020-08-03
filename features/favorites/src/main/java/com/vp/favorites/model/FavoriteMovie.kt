b6282863d736784b6bccae134139a78317b652ee
Robin Lintingre <robin.lintingre@gmail.com>
2020-08-04 01:22:01 +0200

    Challenge #6 : The shrink

--- features/favorites/src/main/java/com/vp/favorites/model/FavoriteMovie.kt
+++ features/favorites/src/main/java/com/vp/favorites/model/FavoriteMovie.kt
@@ -1,9 +1,11 @@
 package com.vp.favorites.model
 
+import com.google.gson.annotations.SerializedName
+
 data class FavoriteMovie(
-        val movieId: String,
-        val title: String,
-        val cover: String
+        @SerializedName ("movieId") val movieId: String,
+        @SerializedName ("title") val title: String,
+        @SerializedName ("cover") val cover: String
 ) {
     fun isValid() = movieId.isNotBlank() && title.isNotBlank() && cover.isNotBlank() && cover != "N/A"
 }
