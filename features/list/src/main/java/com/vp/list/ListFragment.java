a19bc9fbd671584dd9a496dc358affe6c2fd5a3d
JM Vincenti <jean.marie.vincenti@gmail.com>
2020-08-10 13:39:59 +0200

    Open details uri when clicking on ListFragment items

--- features/list/src/main/java/com/vp/list/ListFragment.java
+++ features/list/src/main/java/com/vp/list/ListFragment.java
@@ -1,30 +1,28 @@
 package com.vp.list;
 
-import androidx.lifecycle.ViewModelProvider;
-import androidx.lifecycle.ViewModelProviders;
 import android.content.Intent;
 import android.content.res.Configuration;
 import android.net.Uri;
 import android.os.Bundle;
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-import com.google.android.material.bottomnavigation.BottomNavigationView;
-import androidx.fragment.app.Fragment;
-import androidx.recyclerview.widget.GridLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.ViewAnimator;
-
-import com.vp.list.viewmodel.SearchResult;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
+import androidx.lifecycle.ViewModelProviders;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.vp.list.viewmodel.ListViewModel;
+import com.vp.list.viewmodel.SearchResult;
+import dagger.android.support.AndroidSupportInjection;
 
 import javax.inject.Inject;
-
-import dagger.android.support.AndroidSupportInjection;
 
 public class ListFragment extends Fragment implements GridPagingScrollListener.LoadMoreItemsListener, ListAdapter.OnItemClickListener {
     public static final String TAG = "ListFragment";
@@ -164,6 +162,16 @@ public class ListFragment extends Fragment implements GridPagingScrollListener.L
 
     @Override
     public void onItemClick(String imdbID) {
-        //TODO handle click events
+        Intent intent = new Intent(
+                Intent.ACTION_VIEW,
+                new Uri.Builder()
+                        .scheme("app")
+                        .authority("movies")
+                        .appendPath("details")
+                        .appendQueryParameter("imdbID", imdbID)
+                        .build()
+        );
+        intent.setPackage(requireContext().getPackageName());
+        startActivity(intent);
     }
 }
