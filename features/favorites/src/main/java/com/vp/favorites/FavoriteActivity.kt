d2bb4c2f56e7237c587e056e5cdb8ac3e22d7be7
JM Vincenti <jean.marie.vincenti@gmail.com>
2020-08-11 22:29:21 +0200

    Convert FavoriteActivity to Kotlin

---
+++ features/favorites/src/main/java/com/vp/favorites/FavoriteActivity.kt
@@ -0,0 +1,12 @@
+package com.vp.favorites
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class FavoriteActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_favorite)
+    }
+}
