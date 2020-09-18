d806e273c4043121d974cd1e56cf8e3c8f907c38
OTHMANI <najmothmani@gmail.com>
2020-09-18 17:18:23 +0200

    Fix challenges 1 unit test Fix challenges 3 remove configChanges from list Manifest

--- features/list/src/main/java/com/vp/list/MovieListActivity.java
+++ features/list/src/main/java/com/vp/list/MovieListActivity.java
@@ -1,13 +1,14 @@
 package com.vp.list;
 
 import android.os.Bundle;
-import androidx.fragment.app.Fragment;
-import androidx.appcompat.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.inputmethod.EditorInfo;
 import android.widget.SearchView;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import javax.inject.Inject;
 
@@ -18,7 +19,8 @@ import dagger.android.support.HasSupportFragmentInjector;
 
 public class MovieListActivity extends AppCompatActivity implements HasSupportFragmentInjector {
     private static final String IS_SEARCH_VIEW_ICONIFIED = "is_search_view_iconified";
-
+    private static final String TEXT_SEARCH_QUERY = "text_search_query";
+    String savedQueryText = "";
     @Inject
     DispatchingAndroidInjector<Fragment> dispatchingActivityInjector;
     private SearchView searchView;
@@ -29,6 +31,10 @@ public class MovieListActivity extends AppCompatActivity implements HasSupportFr
         AndroidInjection.inject(this);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_movie_list);
+
+        if (savedInstanceState != null) {
+            savedQueryText = savedInstanceState.getString(TEXT_SEARCH_QUERY);
+        }
 
         if (savedInstanceState == null) {
             getSupportFragmentManager()
@@ -49,6 +55,8 @@ public class MovieListActivity extends AppCompatActivity implements HasSupportFr
         searchView = (SearchView) menuItem.getActionView();
         searchView.setImeOptions(EditorInfo.IME_FLAG_NO_EXTRACT_UI);
         searchView.setIconified(searchViewExpanded);
+        searchView.setQuery(savedQueryText, false);
+
         searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
             @Override
             public boolean onQueryTextSubmit(String query) {
@@ -70,6 +78,7 @@ public class MovieListActivity extends AppCompatActivity implements HasSupportFr
     protected void onSaveInstanceState(Bundle outState) {
         super.onSaveInstanceState(outState);
         outState.putBoolean(IS_SEARCH_VIEW_ICONIFIED, searchView.isIconified());
+        outState.putString(TEXT_SEARCH_QUERY, searchView.getQuery().toString());
     }
 
     @Override
